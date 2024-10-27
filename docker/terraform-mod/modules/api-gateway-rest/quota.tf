c69d695484311babfee5e6a6dcc67c7726e1e3e8
Rafael Mule <15720390+rafael-bm@users.noreply.github.com>
2024-10-27 07:54:58 +0000

    Enable multi quotas for api gateway (#4)

--- docker/terraform-mod/modules/api-gateway-rest/quota.tf
+++ docker/terraform-mod/modules/api-gateway-rest/quota.tf
@@ -1,14 +1,8 @@
-locals {
-  enable_quota = var.quota == null ? false : true
-
-  enable_api_key = local.enable_quota ? try(var.quota.enable_api_key, false) : false
-}
-
 resource "aws_api_gateway_usage_plan" "main" {
-  for_each     = toset(local.enable_quota ? ["singleton"] : [])
+  for_each     = var.quotas
   name         = "${local.prefixed_name}-plan"
   description  = var.description
-  product_code = var.quota.product_code
+  product_code = each.key
   tags         = local.tags
 
   api_stages {
@@ -17,39 +11,39 @@ resource "aws_api_gateway_usage_plan" "main" {
   }
 
   quota_settings {
-    limit  = var.quota.quota_limit
-    offset = var.quota.quota_offset
-    period = var.quota.quota_period
+    limit  = each.value.quota_limit
+    offset = each.value.quota_offset
+    period = each.value.quota_period
   }
 
   throttle_settings {
-    burst_limit = var.quota.throttle_burst_limit
-    rate_limit  = var.quota.throttle_rate_limit
+    burst_limit = each.value.throttle_burst_limit
+    rate_limit  = each.value.throttle_rate_limit
   }
 }
 
 resource "aws_api_gateway_api_key" "main" {
-  for_each = toset(local.enable_api_key ? ["singleton"] : [])
-  name     = "${local.prefixed_name}-apikey"
+  for_each = { for k, v in var.quotas : k => v if v.enable_api_key == true }
+  name     = "${local.prefixed_name}-${each.key}-apikey"
   tags     = local.tags
 }
 
 resource "aws_api_gateway_usage_plan_key" "main" {
-  for_each      = toset(local.enable_api_key ? ["singleton"] : [])
-  key_id        = aws_api_gateway_api_key.main["singleton"].id
+  for_each      = { for k, v in var.quotas : k => v if v.enable_api_key == true }
+  key_id        = aws_api_gateway_api_key.main[each.key].id
   key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.main["singleton"].id
+  usage_plan_id = aws_api_gateway_usage_plan.main[each.key].id
 }
 
 resource "aws_secretsmanager_secret" "main" {
-  for_each    = toset(local.enable_api_key ? ["singleton"] : [])
-  name        = "${local.prefixed_name}-apikey"
+  for_each    = { for k, v in var.quotas : k => v if v.enable_api_key == true }
+  name        = "${local.prefixed_name}-${each.key}-apikey"
   description = var.description
   tags        = var.tags
 }
 
 resource "aws_secretsmanager_secret_version" "main" {
-  for_each      = toset(local.enable_api_key ? ["singleton"] : [])
-  secret_id     = aws_secretsmanager_secret.main["singleton"].id
-  secret_string = aws_api_gateway_usage_plan_key.main["singleton"].value
+  for_each      = { for k, v in var.quotas : k => v if v.enable_api_key == true }
+  secret_id     = aws_secretsmanager_secret.main[each.key].id
+  secret_string = aws_api_gateway_usage_plan_key.main[each.key].value
 }
