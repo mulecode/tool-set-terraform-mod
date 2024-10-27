1cc41ad9254c32b2cc5fe363db5d493c4cd7a2cd
Rafael Mule <15720390+rafael-bm@users.noreply.github.com>
2024-10-27 08:17:30 +0000

    Fix terraform variables to match quotas of api gateway module (#5)

--- docker/terraform-mod/api_gateway_rest.tf
+++ docker/terraform-mod/api_gateway_rest.tf
@@ -7,5 +7,5 @@ module "aws_api_gateway_rest" {
   disable_execute_api_endpoint = each.value.disable_execute_api_endpoint
   api_body                     = templatefile(each.value.api_body, each.value.api_body_params)
   custom_domain                = try(each.value.custom_domain, null)
-  quota                        = each.value.quota
+  quotas                       = each.value.quotas
 }
