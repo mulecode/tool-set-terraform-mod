6a24ea6cbb4f8701a862a758f06ea6f30466a8f1
Rafael Mule <15720390+rafael-bm@users.noreply.github.com>
2024-11-23 09:21:31 +0000

    Linting code

--- docker/terraform-mod/cloudfront.tf
+++ docker/terraform-mod/cloudfront.tf
@@ -10,28 +10,6 @@ module "cloudfront" {
   viewer_certificate     = each.value.viewer_certificate
   default_cache_behavior = each.value.default_cache_behavior
   origin_access_controls = each.value.origin_access_controls
-  custom_error_responses = each.value.custom_error_responses
+
   tags = each.value.tags
-  # origin_bucket = {
-  #   origin_id   = "S3WebsiteBucket"
-  #   domain_name = module.bucket["fortify-guard-cdn"].bucket_regional_domain_name
-  #   bucket_id   = module.bucket["fortify-guard-cdn"].bucket_id
-  #   bucket_arn  = module.bucket["fortify-guard-cdn"].bucket_arn
-  # }
-  #   origin_bucket_website = {
-  #     origin_id   = "S3WebsiteBucket"
-  #     origin_path = "/index.html"
-  #     domain_name = "fortify-guard-cdn-901872380504.s3-website.eu-west-2.amazonaws.com"
-  #   }
-
-  #   origin_apigw = {
-  #     domain_name = replace(module.api_gateway.invoke_url, "/^https?://([^/]*).*/", "$1")
-  #     origin_path = "/api"
-  #   }
-
-  # custom_domain = {
-  #   domain_name         = "${var.cf_custom_domain}.${var.domain_name}"
-  #   zone_id             = data.aws_route53_zone.selected.zone_id
-  #   acm_certificate_arn = data.aws_acm_certificate.global.arn
-  # }
 }
